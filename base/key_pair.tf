e94ea95197f39ee8792c3c7eeffed88d8ab89a70
subeeshr <mrsubeesh@gmail.com>
2019-01-30 13:13:43 +0000

    env base is ready - keypair changed 1

--- base/key_pair.tf
+++ base/key_pair.tf
@@ -5,5 +5,5 @@ resource "tls_private_key" "ref_tls" {
 
 resource "aws_key_pair" "ref_key" {
   key_name   = "${var.key_name}"
-  public_key = "${tls_private_key.example.public_key_openssh}"
+  public_key = "${tls_private_key.ref_tls.public_key_openssh}"
 }
