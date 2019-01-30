397b5629f6d584653584d741d251f8b9d4df68ab
subeeshr <mrsubeesh@gmail.com>
2019-01-30 13:12:45 +0000

    env base is ready - keypair changed

--- base/key_pair.tf
+++ base/key_pair.tf
@@ -1,4 +1,9 @@
+resource "tls_private_key" "ref_tls" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "ref_key" {
   key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key_path)}"
+  public_key = "${tls_private_key.example.public_key_openssh}"
 }
